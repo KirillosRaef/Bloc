8c8ec9abe814982013b82f61ade2279b923738bf
KirillosRaef <Kirillos.RZM@gmail.com>
2024-06-12 14:33:53 +0300

    first commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://nrappfatbxqcoogbxzxz.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yYXBwZmF0YnhxY29vZ2J4enh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0NDA1NTYsImV4cCI6MjAyOTAxNjU1Nn0.uEFitDTmbSFsu_GnUPDiUnX4DgBP9kgIKojB_kZCRMM";
+}
---
+++ lib/init_dependencies.dart
@@ -0,0 +1,135 @@
+import 'package:blog_app/core/cubits/app_user/app_user_cubit.dart';
+import 'package:blog_app/core/network/connection_checker.dart';
+import 'package:blog_app/core/secrets/app_secrets.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
+import 'package:blog_app/features/blog/data/datasources/data_local_data_source.dart';
+import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
+import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
+import 'package:get_it/get_it.dart';
+//import 'package:hive/hive.dart';
+//import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+//import 'package:path_provider/path_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocater = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  _initBlog();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  //Hive.defaultDirectory = (await getApplicationCacheDirectory()).path;
+
+  serviceLocater.registerLazySingleton(() => supabase.client);
+
+  serviceLocater.registerLazySingleton(() => AppUserCubit());
+
+  //serviceLocater.registerLazySingleton(() => Hive.box(name: 'blogs'));
+
+  //serviceLocater.registerFactory(() => InternetConnection());
+
+  // serviceLocater.registerFactory<ConnectionChecker>(
+  //     () => ConnectionCheckerImpl(serviceLocater()));
+}
+
+void _initAuth() {
+  serviceLocater.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(
+      serviceLocater(),
+      //serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory(
+    () => UserSignUp(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory(
+    () => UserLogin(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory(
+    () => CurrentUser(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerLazySingleton(
+    () => AuthBloc(
+      userSignUp: serviceLocater(),
+      userLogin: serviceLocater(),
+      currentUser: serviceLocater(),
+      appUserCubit: serviceLocater(),
+    ),
+  );
+}
+
+void _initBlog() {
+  serviceLocater.registerFactory<BlogRemoteDataSource>(
+    () => BlogRemoteDataSourceImpl(
+      serviceLocater(),
+    ),
+  );
+
+  // serviceLocater.registerFactory<BlogLocalDataSource>(
+  //   () => BlogLocalDataSourceImpl(
+  //     serviceLocater(),
+  //   ),
+  // );
+
+  // serviceLocater.registerFactory<BlogLocalDataSource>(
+  //   () => BlogLocalDataSourceImpl(
+  //     serviceLocater(),
+  //   ),
+  // );
+
+  serviceLocater.registerFactory<BlogRepository>(
+    () => BlogRepositoryImpl(
+      serviceLocater(),
+      //serviceLocater(),
+      //serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory(
+    () => UploadBlog(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerFactory(
+    () => GetAllBlogs(
+      serviceLocater(),
+    ),
+  );
+
+  serviceLocater.registerLazySingleton(
+    () => BlogBloc(
+      uploadBlog: serviceLocater(),
+      getAllBlogs: serviceLocater(),
+    ),
+  );
+}
